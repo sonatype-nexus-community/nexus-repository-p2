b35d40f3b1a8c6e78eb634c42e51da44d13058a7
DarthHater <jeffryxtron@gmail.com>
2017-06-01 12:59:35 -0800

    Public export of Nexus Repository - P2 Plugin:
      - 5c3e885c1c9086e150cd652070c977aab2f41cb1 by DarthHater <jeffryxtron@gmail.com>
    
    GitOrigin-RevId: 5c3e885c1c9086e150cd652070c977aab2f41cb1

---
+++ src/main/java/org/sonatype/nexus/repository/p2/internal/proxy/P2ProxyRecipe.groovy
@@ -0,0 +1,224 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2017-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.p2.internal.proxy
+
+import javax.annotation.Nonnull
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Provider
+import javax.inject.Singleton
+
+import org.sonatype.nexus.repository.Format
+import org.sonatype.nexus.repository.RecipeSupport
+import org.sonatype.nexus.repository.Repository
+import org.sonatype.nexus.repository.Type
+import org.sonatype.nexus.repository.attributes.AttributesFacet
+import org.sonatype.nexus.repository.cache.NegativeCacheFacet
+import org.sonatype.nexus.repository.cache.NegativeCacheHandler
+import org.sonatype.nexus.repository.http.HttpHandlers
+import org.sonatype.nexus.repository.http.PartialFetchHandler
+import org.sonatype.nexus.repository.httpclient.HttpClientFacet
+import org.sonatype.nexus.repository.p2.internal.AssetKind
+import org.sonatype.nexus.repository.p2.internal.P2Format
+import org.sonatype.nexus.repository.p2.internal.security.P2SecurityFacet
+import org.sonatype.nexus.repository.proxy.ProxyHandler
+import org.sonatype.nexus.repository.search.SearchFacet
+import org.sonatype.nexus.repository.security.SecurityHandler
+import org.sonatype.nexus.repository.storage.DefaultComponentMaintenanceImpl
+import org.sonatype.nexus.repository.storage.StorageFacet
+import org.sonatype.nexus.repository.storage.UnitOfWorkHandler
+import org.sonatype.nexus.repository.types.ProxyType
+import org.sonatype.nexus.repository.purge.PurgeUnusedFacet
+import org.sonatype.nexus.repository.view.ConfigurableViewFacet
+import org.sonatype.nexus.repository.view.Context
+import org.sonatype.nexus.repository.view.Route
+import org.sonatype.nexus.repository.view.Route.Builder
+import org.sonatype.nexus.repository.view.Router
+import org.sonatype.nexus.repository.view.ViewFacet
+import org.sonatype.nexus.repository.view.handlers.BrowseUnsupportedHandler
+import org.sonatype.nexus.repository.view.handlers.ConditionalRequestHandler
+import org.sonatype.nexus.repository.view.handlers.ContentHeadersHandler
+import org.sonatype.nexus.repository.view.handlers.ExceptionHandler
+import org.sonatype.nexus.repository.view.handlers.HandlerContributor
+import org.sonatype.nexus.repository.view.handlers.TimingHandler
+import org.sonatype.nexus.repository.view.matchers.ActionMatcher
+import org.sonatype.nexus.repository.view.matchers.logic.LogicMatchers
+import org.sonatype.nexus.repository.view.matchers.token.TokenMatcher
+
+import static org.sonatype.nexus.repository.http.HttpMethods.GET
+import static org.sonatype.nexus.repository.http.HttpMethods.HEAD
+
+/**
+ * P2 proxy repository recipe.
+ */
+@Named(P2ProxyRecipe.NAME)
+@Singleton
+class P2ProxyRecipe
+    extends RecipeSupport
+{
+  public static final String NAME = 'p2-proxy'
+
+  @Inject
+  Provider<P2SecurityFacet> securityFacet
+
+  @Inject
+  Provider<ConfigurableViewFacet> viewFacet
+
+  @Inject
+  Provider<StorageFacet> storageFacet
+
+  @Inject
+  Provider<SearchFacet> searchFacet
+
+  @Inject
+  Provider<AttributesFacet> attributesFacet
+
+  @Inject
+  ExceptionHandler exceptionHandler
+
+  @Inject
+  TimingHandler timingHandler
+
+  @Inject
+  SecurityHandler securityHandler
+
+  @Inject
+  PartialFetchHandler partialFetchHandler
+
+  @Inject
+  ConditionalRequestHandler conditionalRequestHandler
+
+  @Inject
+  ContentHeadersHandler contentHeadersHandler
+
+  @Inject
+  UnitOfWorkHandler unitOfWorkHandler
+
+  @Inject
+  BrowseUnsupportedHandler browseUnsupportedHandler
+
+  @Inject
+  HandlerContributor handlerContributor
+
+  @Inject
+  Provider<DefaultComponentMaintenanceImpl> componentMaintenanceFacet
+
+  @Inject
+  Provider<HttpClientFacet> httpClientFacet
+
+  @Inject
+  Provider<P2ProxyFacetImpl> proxyFacet
+
+  @Inject
+  Provider<NegativeCacheFacet> negativeCacheFacet
+
+  @Inject
+  Provider<PurgeUnusedFacet> purgeUnusedFacet
+
+  @Inject
+  NegativeCacheHandler negativeCacheHandler
+
+  @Inject
+  ProxyHandler proxyHandler
+
+  @Inject
+  P2ProxyRecipe(@Named(ProxyType.NAME) final Type type, @Named(P2Format.NAME) final Format format) {
+    super(type, format)
+  }
+
+  @Override
+  void apply(@Nonnull final Repository repository) throws Exception {
+    repository.attach(securityFacet.get())
+    repository.attach(configure(viewFacet.get()))
+    repository.attach(httpClientFacet.get())
+    repository.attach(negativeCacheFacet.get())
+    repository.attach(componentMaintenanceFacet.get())
+    repository.attach(proxyFacet.get())
+    repository.attach(storageFacet.get())
+    repository.attach(searchFacet.get())
+    repository.attach(purgeUnusedFacet.get())
+    repository.attach(attributesFacet.get())
+  }
+
+  Closure assetKindHandler = { Context context, AssetKind value ->
+    context.attributes.set(org.sonatype.nexus.repository.p2.internal.AssetKind, value)
+    return context.proceed()
+  }
+
+  /**
+   * Matcher for packages mapping.
+   */
+  static Builder packagesMatcher() {
+    new Builder().matcher(
+        LogicMatchers.and(
+            new ActionMatcher(GET, HEAD),
+            new TokenMatcher('/{path:.+}/PACKAGES.gz')
+        ))
+  }
+
+  /**
+   * Matcher for archive mapping.
+   */
+  static Builder archiveMatcher() {
+    new Builder().matcher(
+        LogicMatchers.and(
+            new ActionMatcher(GET, HEAD),
+            new TokenMatcher('/{path:.+}/{filename:.+}')
+        ))
+  }
+
+  /**
+   * Configure {@link ViewFacet}.
+   */
+  private ViewFacet configure(final ConfigurableViewFacet facet) {
+    Router.Builder builder = new Router.Builder()
+
+    builder.route(packagesMatcher()
+        .handler(timingHandler)
+        .handler(assetKindHandler.rcurry(PACKAGES))
+        .handler(securityHandler)
+        .handler(exceptionHandler)
+        .handler(handlerContributor)
+        .handler(negativeCacheHandler)
+        .handler(partialFetchHandler)
+        .handler(contentHeadersHandler)
+        .handler(unitOfWorkHandler)
+        .handler(proxyHandler)
+        .create())
+
+    builder.route(archiveMatcher()
+        .handler(timingHandler)
+        .handler(assetKindHandler.rcurry(ARCHIVE))
+        .handler(securityHandler)
+        .handler(exceptionHandler)
+        .handler(handlerContributor)
+        .handler(negativeCacheHandler)
+        .handler(conditionalRequestHandler)
+        .handler(partialFetchHandler)
+        .handler(contentHeadersHandler)
+        .handler(unitOfWorkHandler)
+        .handler(proxyHandler)
+        .create())
+
+    builder.route(new Route.Builder()
+        .matcher(BrowseUnsupportedHandler.MATCHER)
+        .handler(browseUnsupportedHandler)
+        .create())
+
+    builder.defaultHandlers(HttpHandlers.notFound())
+
+    facet.configure(builder.create())
+
+    return facet
+  }
+}
