7a9ecdb15eb5742dfb9f7bc8ae9582ba3dc83378
DarthHater <jeffryxtron@gmail.com>
2017-06-01 16:16:00 -0800

    Public export of Nexus Repository - P2 Plugin:
      - 02fa9f43372addc477b1b033881aac915a5f72f7 by DarthHater <jeffryxtron@gmail.com>
    
    GitOrigin-RevId: 02fa9f43372addc477b1b033881aac915a5f72f7

--- src/main/java/org/sonatype/nexus/repository/p2/internal/proxy/P2ProxyFacetImpl.java
+++ src/main/java/org/sonatype/nexus/repository/p2/internal/proxy/P2ProxyFacetImpl.java
@@ -21,15 +21,28 @@ import javax.inject.Named;
 
 import org.sonatype.nexus.repository.cache.CacheInfo;
 import org.sonatype.nexus.repository.config.Configuration;
+import org.sonatype.nexus.repository.p2.internal.util.P2DataAccess;
 import org.sonatype.nexus.repository.p2.internal.util.P2PathUtils;
 import org.sonatype.nexus.repository.proxy.ProxyFacet;
 import org.sonatype.nexus.repository.proxy.ProxyFacetSupport;
+import org.sonatype.nexus.repository.storage.Asset;
+import org.sonatype.nexus.repository.storage.Bucket;
+import org.sonatype.nexus.repository.storage.Component;
+import org.sonatype.nexus.repository.storage.StorageFacet;
+import org.sonatype.nexus.repository.storage.StorageTx;
+import org.sonatype.nexus.repository.storage.TempBlob;
+import org.sonatype.nexus.repository.transaction.TransactionalStoreBlob;
+import org.sonatype.nexus.repository.transaction.TransactionalTouchBlob;
+import org.sonatype.nexus.repository.transaction.TransactionalTouchMetadata;
 import org.sonatype.nexus.repository.view.Content;
 import org.sonatype.nexus.repository.view.Context;
 import org.sonatype.nexus.repository.p2.internal.AssetKind;
+import org.sonatype.nexus.repository.view.Payload;
 import org.sonatype.nexus.repository.view.matchers.token.TokenMatcher;
+import org.sonatype.nexus.transaction.UnitOfWork;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sonatype.nexus.repository.storage.AssetEntityAdapter.P_ASSET_KIND;
 
 /**
  * P2 {@link ProxyFacet} implementation.
@@ -39,10 +52,12 @@ public class P2ProxyFacetImpl
     extends ProxyFacetSupport
 {
   private final P2PathUtils p2PathUtils;
+  private final P2DataAccess p2DataAccess;
 
   @Inject
-  public P2ProxyFacetImpl(final P2PathUtils p2PathUtils) {
+  public P2ProxyFacetImpl(final P2PathUtils p2PathUtils, final P2DataAccess p2DataAccess) {
     this.p2PathUtils = checkNotNull(p2PathUtils);
+    this.p2DataAccess = checkNotNull(p2DataAccess);
   }
 
   // HACK: Workaround for known CGLIB issue, forces an Import-Package for org.sonatype.nexus.repository.config
@@ -56,23 +71,153 @@ public class P2ProxyFacetImpl
   protected Content getCachedContent(final Context context) throws IOException {
     AssetKind assetKind = context.getAttributes().require(AssetKind.class);
     TokenMatcher.State matcherState = p2PathUtils.matcherState(context);
-    return null;
+    switch(assetKind) {
+      case ARTIFACT_JAR:
+      case ARTIFACT_XML:
+      case ARTIFACT_XML_XZ:
+      case CONTENT_JAR:
+      case CONTENT_XML:
+      case CONTENT_XML_XZ:
+      case P2_INDEX:
+        return getAsset(p2PathUtils.path(matcherState));
+      case COMPONENT_PLUGINS_JAR:
+      case COMPONENT_FEATURES_JAR:
+      case COMPONENT_PLUGINS_PACK_GZ:
+      case COMPONENT_FEATURES_PACK_GZ:
+        return getAsset(p2PathUtils.path(p2PathUtils.path(matcherState), p2PathUtils.filename(matcherState)));
+      default:
+        throw new IllegalStateException();
+    }
   }
 
   @Override
   protected Content store(final Context context, final Content content) throws IOException {
-    return null;
+    AssetKind assetKind = context.getAttributes().require(AssetKind.class);
+    TokenMatcher.State matcherState = p2PathUtils.matcherState(context);
+    switch(assetKind) {
+      case ARTIFACT_JAR:
+      case ARTIFACT_XML:
+      case ARTIFACT_XML_XZ:
+      case CONTENT_JAR:
+      case CONTENT_XML:
+      case CONTENT_XML_XZ:
+      case P2_INDEX:
+        return putMetadata(p2PathUtils.path(matcherState), content, assetKind);
+      case COMPONENT_PLUGINS_JAR:
+      case COMPONENT_FEATURES_JAR:
+      case COMPONENT_PLUGINS_PACK_GZ:
+      case COMPONENT_FEATURES_PACK_GZ:
+        return putComponent(p2PathUtils.path(matcherState),
+            p2PathUtils.filename(matcherState),
+            content,
+            assetKind);
+      default:
+        throw new IllegalStateException();
+    }
+  }
+
+  private Content putMetadata(final String path, final Content content, final AssetKind assetKind) throws IOException {
+    StorageFacet storageFacet = facet(StorageFacet.class);
+    try (TempBlob tempBlob = storageFacet.createTempBlob(content.openInputStream(), P2DataAccess.HASH_ALGORITHMS)) {
+      return doPutMetadata(path, tempBlob, content, assetKind);
+    }
+  }
+
+  @TransactionalStoreBlob
+  protected Content doPutMetadata(final String path,
+                                  final TempBlob metadataContent,
+                                  final Payload payload,
+                                  final AssetKind assetKind) throws IOException {
+    StorageTx tx = UnitOfWork.currentTx();
+    Bucket bucket = tx.findBucket(getRepository());
+
+    String assetPath = path;
+
+    Asset asset = p2DataAccess.findAsset(tx, bucket, assetPath);
+    if (asset == null) {
+      asset = tx.createAsset(bucket, getRepository().getFormat());
+      asset.name(assetPath);
+      asset.formatAttributes().set(P_ASSET_KIND, assetKind.name());
+    }
+
+    return p2DataAccess.saveAsset(tx, asset, metadataContent, payload);
+  }
+
+  private Content putComponent(final String path,
+                               final String filename,
+                               final Content content,
+                               final AssetKind assetKind) throws IOException {
+    StorageFacet storageFacet = facet(StorageFacet.class);
+    try (TempBlob tempBlob = storageFacet.createTempBlob(content.openInputStream(), P2DataAccess.HASH_ALGORITHMS)) {
+      return doPutComponent(path, filename, tempBlob, content, assetKind);
+    }
+  }
+
+  protected Content doPutComponent(final String path,
+                                   final String filename,
+                                   final TempBlob componentContent,
+                                   final Payload payload,
+                                   final AssetKind assetKind) throws IOException {
+    StorageTx tx = UnitOfWork.currentTx();
+    Bucket bucket = tx.findBucket(getRepository());
+    String assetPath = p2PathUtils.path(path, filename);
+
+
+    Component component = p2DataAccess.findComponent(tx, getRepository(), filename, "version");
+    if (component == null) {
+      component = tx.createComponent(bucket, getRepository().getFormat())
+          .name(filename)
+          .version("version");
+    }
+    tx.saveComponent(component);
+
+    Asset asset = p2DataAccess.findAsset(tx, bucket, assetPath);
+    if (asset == null) {
+      asset = tx.createAsset(bucket, component);
+      asset.name(assetPath);
+      asset.formatAttributes().set(P_ASSET_KIND, assetKind.name());
+    }
+    return p2DataAccess.saveAsset(tx, asset, componentContent, payload);
   }
 
   @Override
   protected void indicateVerified(final Context context, final Content content, final CacheInfo cacheInfo)
       throws IOException
   {
+    setCacheInfo(content, cacheInfo);
+  }
 
+  @TransactionalTouchMetadata
+  public void setCacheInfo(final Content content, final CacheInfo cacheInfo) throws IOException {
+    StorageTx tx = UnitOfWork.currentTx();
+    Asset asset = Content.findAsset(tx, tx.findBucket(getRepository()), content);
+    if (asset == null) {
+      log.debug(
+          "Attempting to set cache info for non-existent P2 asset {}", content.getAttributes().require(Asset.class)
+      );
+      return;
+    }
+    log.debug("Updating cacheInfo of {} to {}", asset, cacheInfo);
+    CacheInfo.applyToAsset(asset, cacheInfo);
+    tx.saveAsset(asset);
+  }
+
+  @TransactionalTouchBlob
+  protected Content getAsset(final String name) {
+    StorageTx tx = UnitOfWork.currentTx();
+
+    Asset asset = p2DataAccess.findAsset(tx, tx.findBucket(getRepository()), name);
+    if (asset == null) {
+      return null;
+    }
+    if (asset.markAsDownloaded()) {
+      tx.saveAsset(asset);
+    }
+    return p2DataAccess.toContent(asset, tx.requireBlob(asset.requireBlobRef()));
   }
 
   @Override
   protected String getUrl(@Nonnull final Context context) {
-    return null;
+    return context.getRequest().getPath().substring(1);
   }
 }
---
+++ src/test/java/org/sonatype/nexus/repository/p2/internal/util/P2DataAccessTest.java
@@ -0,0 +1,100 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+
+package org.sonatype.nexus.repository.p2.internal.util;
+
+import java.util.List;
+
+import org.sonatype.goodies.testsupport.TestSupport;
+import org.sonatype.nexus.repository.Repository;
+import org.sonatype.nexus.repository.storage.Asset;
+import org.sonatype.nexus.repository.storage.Bucket;
+import org.sonatype.nexus.repository.storage.Component;
+import org.sonatype.nexus.repository.storage.StorageTx;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.sonatype.nexus.common.hash.HashAlgorithm.SHA1;
+
+/**
+ * Test for {@link P2DataAccess}
+ */
+public class P2DataAccessTest
+    extends TestSupport
+{
+  private final String assetName = "test";
+  @Mock
+  StorageTx tx;
+
+  @Mock
+  Repository repository;
+
+  @Mock
+  Component component;
+
+  @Mock
+  Asset asset;
+
+  @Mock
+  Bucket bucket;
+
+  P2DataAccess underTest;
+
+  @Before
+  public void setUp() throws Exception {
+    underTest = new P2DataAccess();
+  }
+
+  @Test
+  public void immutableListIsSha1() {
+    assertThat(P2DataAccess.HASH_ALGORITHMS, is(equalTo(ImmutableList.of(SHA1))));
+  }
+
+  @Test
+  public void findComponent() throws Exception {
+    List<Component> list = ImmutableList.of(component);
+    when(tx.findComponents(any(), any()))
+        .thenReturn(list);
+
+    assertThat(underTest.findComponent(tx, repository, "test", "test"), is(equalTo(component)));
+  }
+
+  @Test
+  public void findAsset() throws Exception {
+    when(tx.findAssetWithProperty(any(), any(), any(Bucket.class)))
+        .thenReturn(asset);
+
+    assertThat(underTest.findAsset(tx, bucket, assetName), is(equalTo(asset)));
+  }
+
+  @Test
+  public void saveAsset() throws Exception {
+  }
+
+  @Test
+  public void saveAsset1() throws Exception {
+  }
+
+  @Test
+  public void toContent() throws Exception {
+  }
+
+}
