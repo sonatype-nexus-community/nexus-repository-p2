98f0ab41eaf4601aefed449ec7bc123cdf5161ec
allenhsieh <allenhsiehhr@gmail.com>
2019-09-26 15:49:47 -0700

    initial IT for ArtifactsXmlAbsoluteUrlRemover

--- nexus-repository-p2-it/src/test/java/org/sonatype/nexus/repository/p2/internal/P2ProxyIT.java
+++ nexus-repository-p2-it/src/test/java/org/sonatype/nexus/repository/p2/internal/P2ProxyIT.java
@@ -12,6 +12,10 @@
  */
 package org.sonatype.nexus.repository.p2.internal;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import org.sonatype.goodies.httpfixture.server.fluent.Behaviours;
 import org.sonatype.goodies.httpfixture.server.fluent.Server;
 import org.sonatype.nexus.pax.exam.NexusPaxExamSupport;
@@ -21,6 +25,9 @@ import org.sonatype.nexus.repository.storage.Asset;
 import org.sonatype.nexus.repository.storage.Component;
 import org.sonatype.nexus.testsuite.testsupport.NexusITSupport;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,7 +37,9 @@ import org.ops4j.pax.exam.Option;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.text.IsEqualIgnoringWhiteSpace.equalToIgnoringWhiteSpace;
 import static org.sonatype.nexus.testsuite.testsupport.FormatClientSupport.status;
+
 
 public class P2ProxyIT
     extends P2ITSupport
@@ -41,21 +50,37 @@ public class P2ProxyIT
 
   private static final String COMPONENT_NAME = "org.eclipse.cvs.source";
 
+  private static final String ARTIFACT_NAME = "artifacts";
+
+  private static final String ARTIFACT_WITHOUT_MIRROR_NAME = "artifacts-mirror-removed";
+
   private static final String VERSION_NUMBER = "1.4.404.v20180330-0640";
 
-  private static final String EXTENSION = ".jar";
+  private static final String EXTENSION_JAR = ".jar";
 
-  private static final String PACKAGE_NAME = COMPONENT_NAME + "_" + VERSION_NUMBER + EXTENSION;
+  private static final String EXTENSION_XML = ".xml";
+
+  private static final String EXTENSION_XML_XZ = ".xml.xz";
+
+  private static final String PACKAGE_NAME = COMPONENT_NAME + "_" + VERSION_NUMBER + EXTENSION_JAR;
+
+  private static final String ARTIFACT_JAR = ARTIFACT_NAME + EXTENSION_JAR;
+
+  private static final String ARTIFACT_XML = ARTIFACT_NAME + EXTENSION_XML;
+
+  private static final String ARTIFACT_XML_XZ = ARTIFACT_NAME + EXTENSION_XML_XZ;
+
+  private static final String ARTIFACT_WITHOUT_MIRROR_XML = ARTIFACT_WITHOUT_MIRROR_NAME + EXTENSION_XML;
 
   private static final String INVALID_PACKAGE_NAME = COMPONENT_NAME + "-0.24.zip";
 
-  private static final String BASE_PATH = "R-4.7.3a-201803300640/features/";
+  private static final String PACKAGE_BASE_PATH = "R-4.7.3a-201803300640/features/";
 
   private static final String BAD_PATH = "/this/path/is/not/valid";
 
-  private static final String VALID_PACKAGE_URL = BASE_PATH + PACKAGE_NAME;
+  private static final String VALID_PACKAGE_URL = PACKAGE_BASE_PATH + PACKAGE_NAME;
 
-  private static final String INVALID_PACKAGE_URL = BASE_PATH + INVALID_PACKAGE_NAME;
+  private static final String INVALID_PACKAGE_URL = PACKAGE_BASE_PATH + INVALID_PACKAGE_NAME;
 
   private P2Client proxyClient;
 
@@ -76,6 +101,12 @@ public class P2ProxyIT
     server = Server.withPort(0)
         .serve("/" + VALID_PACKAGE_URL)
         .withBehaviours(Behaviours.file(testData.resolveFile(PACKAGE_NAME)))
+        .serve("/" + ARTIFACT_JAR)
+        .withBehaviours(Behaviours.file(testData.resolveFile(ARTIFACT_JAR)))
+        .serve("/" + ARTIFACT_XML)
+        .withBehaviours(Behaviours.file(testData.resolveFile(ARTIFACT_XML)))
+        .serve("/" + ARTIFACT_XML_XZ)
+        .withBehaviours(Behaviours.file(testData.resolveFile(ARTIFACT_XML_XZ)))
         .start();
 
     proxyRepo = repos.createP2Proxy("p2-test-proxy", server.getUrl().toExternalForm());
@@ -91,8 +122,8 @@ public class P2ProxyIT
   public void retrieveJarFromProxyWhenRemoteOnline() throws Exception {
     assertThat(status(proxyClient.get(VALID_PACKAGE_URL)), is(HttpStatus.OK));
 
-    final Asset asset = findAsset(proxyRepo, VALID_PACKAGE_URL);
-    assertThat(asset.name(), is(equalTo(VALID_PACKAGE_URL)));
+    final Asset asset = findAsset(proxyRepo, "/" + VALID_PACKAGE_URL);
+    assertThat(asset.name(), is(equalTo("/" + VALID_PACKAGE_URL)));
     assertThat(asset.contentType(), is(equalTo(MIME_TYPE)));
     assertThat(asset.format(), is(equalTo(FORMAT_NAME)));
 
@@ -104,6 +135,22 @@ public class P2ProxyIT
   @Test
   public void retrieveZipFromProxyShouldNotWork() throws Exception {
     assertThat(status(proxyClient.get(INVALID_PACKAGE_URL)), is(HttpStatus.NOT_FOUND));
+  }
+
+  @Test
+  public void removeMirrorURLFromXML() throws Exception {
+    assertThat(status(proxyClient.get(ARTIFACT_XML)), is(HttpStatus.OK));
+
+    try (CloseableHttpResponse response = proxyClient.get(ARTIFACT_XML)) {
+      HttpEntity entity = response.getEntity();
+      String result = IOUtils.toString(entity.getContent()).replaceAll("\\s+","");
+
+      InputStream targetStream = new FileInputStream(testData.resolveFile(ARTIFACT_WITHOUT_MIRROR_XML));
+      String expected = IOUtils.toString(targetStream).replaceAll("\\s+","");
+
+      assertThat(result, equalTo(expected));
+
+    }
   }
 
   @After
